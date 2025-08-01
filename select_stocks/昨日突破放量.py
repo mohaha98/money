e9726225c0096d66c111022510fba0900a9b1a7f
mojh <2351479720@qq.com>
2025-08-01 19:40:47 +0800

    2025/08/01 19:40:44  备注:

--- select_stocks/昨日突破放量.py
+++ select_stocks/昨日突破放量.py
@@ -27,6 +27,7 @@ def is_breakout_volume_01(df):
 
 def select_stocks():
     """主函数：筛选符合条件的股票"""
+
     stock_list = filter_stocks(LB_min=1.5, HSL_min=2, close_min=12)
     # stock_list=['600557']
     result = []
@@ -43,5 +44,5 @@ if __name__  ==  '__main__':
     now = datetime.today().strftime('%Y%m%d%H%M')
     log.info(f'长度是{len(code)}')
     log.info(f'{code}')
-    send_email(f'{now}昨日突破放量---： \n\n\n\n'+str(code))
+    # send_email(f'{now}昨日突破放量---： \n\n\n\n'+str(code))
 
