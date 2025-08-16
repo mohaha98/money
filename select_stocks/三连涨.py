fc547c0bc75f3add8c47efb5e146dd1a89f5baa2
mojh <2351479720@qq.com>
2025-08-16 12:57:57 +0800

    2025/08/16 12:57:56  备注:55

--- select_stocks/三连涨.py
+++ select_stocks/三连涨.py
@@ -46,7 +46,7 @@ def is_up3_mild_trend(df):
 def select_stocks():
 
     """主函数：筛选符合条件的股票"""
-    stock_list = filter_stocks(close_min=15,SZ_min=99)
+    stock_list = filter_stocks(close_min=15,SZ_min=120)
     # stock_list=['601311']
     result = []
     for code in tqdm(stock_list, desc="选股进度", bar_format="{l_bar}{bar:30}{r_bar}", colour="green"):
--- stocks_information/板块搜索.py
+++ stocks_information/板块搜索.py
@@ -97,5 +97,5 @@ def rqlt_dc(code):
 
 if __name__ == '__main__':
     pass
-    get_bkid('AI')
+    get_bkid('军工')
 
